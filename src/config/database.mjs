bcacbba603e7767afb325d5f15758fdec73b4ecd
benjaUnifranz <cbbe.benjamin.rodriguez.or@unifranz.edu.bo>
2025-02-22 19:22:22 -0400

    estoy cansado de esta decidia mia

--- src/config/database.mjs
+++ src/config/database.mjs
@@ -5,14 +5,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.POSTGRES_DB || 'viaje',
+  user: process.env.POSTGRES_USER || 'benjaminRo',
+  password: process.env.POSTGRES_PASSWORD || 'G7h!kL9@pQwZ',
   // Agregar search_path al esquema "viajes" 
   options: '-c search_path=viajes,public '
-
 });
   
 // const additionalPool = new Pool({
